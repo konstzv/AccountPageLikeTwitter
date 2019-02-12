9a95d670f5a01d124a1af02491010d829e9df1f4
Konstantin Zagulin <konstzv@gmail.com>
2019-02-13 00:07:30 +0300

    Profile Fragment first version

--- app/src/main/java/ru/zagulin/twitterlikeaccountpage/ProfileFragment.java
+++ app/src/main/java/ru/zagulin/twitterlikeaccountpage/ProfileFragment.java
@@ -2,6 +2,9 @@ package ru.zagulin.twitterlikeaccountpage;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FileDownloadTask;
@@ -20,6 +23,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -28,6 +32,7 @@ import java.io.IOException;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -47,6 +52,20 @@ public class ProfileFragment extends Fragment {
 
     private String mProfileImageLocalFilePath;
 
+    private TextView mEmailTextView;
+
+    private TextView mMTitleTextView;
+
+    private CollapsingToolbarLayout mCollapsingToolbar;
+
+    private AppBarLayout mAppbar;
+
+    private String mUserEmail;
+
+    private ViewPager mViewPager;
+
+    private TabLayout mTabLayout;
+
 
     @Nullable
     @Override
@@ -59,21 +78,8 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
 
-        mImageView = view.findViewById(R.id.fragment_profile_image);
-        mImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(
-                        Intent.createChooser(intent, getString(R.string.select_profile_picture)),
-                        IMG_REQ_CODE);
-            }
-        });
-
-        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
         if (currentUser == null) {
             Activity activity = getActivity();
             if (activity instanceof IAuthCallback) {
@@ -82,8 +88,77 @@ public class ProfileFragment extends Fragment {
             return;
         }
 
+        findViews(view);
+        setListeners();
+
+        setupTabs();
+
         mUserImageLocationInStorage = String.format("images/profiles/%s.jpg", currentUser.getUid());
+
         loadAndDisplayProfileImage();
+
+        mUserEmail = currentUser.getEmail();
+
+        mEmailTextView.setText(mUserEmail);
+
+    }
+
+    private void findViews(@NonNull final View view) {
+        mImageView = view.findViewById(R.id.fragment_profile_image_view_profile);
+        mEmailTextView = view.findViewById(R.id.fragment_profile_text_view_email);
+        mMTitleTextView = view.findViewById(R.id.fragment_profile_text_view_title);
+        mCollapsingToolbar = view.findViewById(R.id.fragment_profile_toolbar_layout);
+        mAppbar = view.findViewById(R.id.fragment_profile_appbar);
+        mViewPager = view.findViewById(R.id.fragment_profile_view_pager);
+        mTabLayout = view.findViewById(R.id.fragment_profile_tab_layout);
+    }
+
+    private void setListeners() {
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                startImageChooser();
+            }
+        });
+
+        mAppbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(final AppBarLayout appBarLayout, final int verticalOffset) {
+
+                actionOnOffsetChange(verticalOffset);
+
+
+            }
+        });
+    }
+
+    private void actionOnOffsetChange(final int verticalOffset) {
+        boolean isContentHide = mCollapsingToolbar.getScrimVisibleHeightTrigger() + Math.abs(verticalOffset) > mCollapsingToolbar
+                .getHeight();
+        if (isContentHide){
+            mMTitleTextView.setText(mUserEmail);
+        }else{
+            mMTitleTextView.setText("");
+        }
+        float scaleFactor = 1F - verticalOffset * .005F ;
+        mImageView.setScaleX(1/scaleFactor);
+        mImageView.setScaleY(1/scaleFactor);
+    }
+
+    private void startImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(intent, getString(R.string.select_profile_picture)),
+                IMG_REQ_CODE);
+    }
+
+    private void setupTabs() {
+
+        mViewPager.setAdapter(
+                new PageAdapter(getChildFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
